eedf87379e26ae187ecd87851b9aae23f120bc11
Tom Wimmenhove <tom.wimmenhove@gmail.com>
2017-09-14 21:38:05 +0000

    Combined demod and rtlfloats

--- demod.c
+++ demod.c
@@ -9,6 +9,9 @@
 #include <string.h>
 #include <ctype.h>
 
+#include <rtl-sdr.h>
+#include <pthread.h>
+
 #include "hex.h"
 #include "protocol.h"
 #include "filter.h"
@@ -63,15 +66,55 @@ double runningPeak(double sample, double *history, int historyPos, int len, doub
 	return currentPeak;
 }
 
+#define DEFAULT_BUF_LENGTH              (16 * 16384)
+
+
 typedef struct
 {
 	float i;
 	float q;
-} __attribute__((packed)) complexSample;
+} __attribute__((packed)) ComplexSample;
+
+typedef struct
+{
+	int fd;
+	rtlsdr_dev_t *dev;
+} Context;
+
+void rtlsdr_callback(unsigned char *buf, uint32_t len, void *c)
+{
+	Context* ctx = (Context*) c;
+
+	if (write(ctx->fd, buf, len) == -1)
+	{
+		perror("write():");
+		exit(1);
+	}
+}
+
+void *startSampler(void *c)
+{
+	Context* ctx = (Context*) c;
+
+	int ret = rtlsdr_read_async(ctx->dev, rtlsdr_callback, c, 0, DEFAULT_BUF_LENGTH);
+	if (ret == -1)
+	{
+		fprintf(stderr, "rtlsdr_read_async returned with %d\n", ret);
+		exit(1);
+	}
+
+}
 
 int main(int argc, char **argv)
 {
-	complexSample buf[128];
+	rtlsdr_dev_t *dev = NULL;
+	unsigned int dev_index = 0;
+	int ret; 
+	uint32_t sampleRate = 1000000;
+	double freq = 433.88e+6;
+	unsigned short **buffer;
+	unsigned short buf[128];
+	
         int samplesPerBit = 40;
 	int preambleMaxError = samplesPerBit / 10.0;
 	int sampleAt = 0.0;
@@ -98,6 +141,14 @@ int main(int argc, char **argv)
 	int decimation = 25;
 	int decimator = 0;
 
+	if (argc < 2)
+	{
+		fprintf(stderr, "Usage: %s <frequency>\n", argv[0]);
+		return -1;
+	}
+
+	freq = atof(argv[1]);
+
 	SampleFilter filteri;
 	SampleFilter filterq;
 	SampleFilter_init(&filteri);
@@ -107,19 +158,66 @@ int main(int argc, char **argv)
 
 	double avg = 0.0;
 
-	int bit = 0;
-	while ((n = read(0, buf, sizeof(buf))) > 0)
+	ComplexSample lut[0x10000];;
+	for (unsigned int i = 0; i < 0x10000; i++)
 	{
-		for (int i = 0 ; i < n / sizeof(complexSample); i++)
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+		lut[i].i = (((float)(i & 0xff)) - 127.4f) * (1.0f/128.0f);
+		lut[i].q = (((float)(i >> 8)) - 127.4f) * (1.0f/128.0f);
+#else // BIG_ENDIAN
+		lut[i].i = (((float)(i >> 8)) - 127.4f) * (1.0f/128.0f);
+		lut[i].q = (((float)(i & 0xff) - 127.4f) * (1.0f/128.0f);
+#endif
+	}
+
+	/* Open the rtlsdr */
+	if (rtlsdr_open(&dev, dev_index) == -1)
+	{
+		fprintf(stderr, "Can't open device");
+		return -1;
+	}
+
+	/* Set some shit */
+	rtlsdr_set_tuner_gain_mode(dev, 1);
+	rtlsdr_set_agc_mode(dev, 0);
+	rtlsdr_reset_buffer(dev);
+	rtlsdr_set_tuner_gain_mode(dev, 0);
+	rtlsdr_set_agc_mode(dev, 1);
+	rtlsdr_set_sample_rate(dev, sampleRate);
+	rtlsdr_set_center_freq(dev, (uint32_t) freq);
+
+
+	/* create our data pipe */
+	int pipefd[2];
+	if (pipe(pipefd) == -1)
+	{
+		perror("pipe()");
+		return -1;
+	}
+	Context ctx;
+	ctx.fd = pipefd[1];
+	ctx.dev = dev;
+
+	pthread_t sampleThread;
+	if (pthread_create(&sampleThread, NULL, startSampler, &ctx) != 0)
+	{
+		perror("pthread_create()");
+		return -1;
+	}
+
+	int bit = 0;
+	while ((n = read(pipefd[0], buf, sizeof(buf))) > 0)
+	{
+		int nout = n / 2;
+		for (int i = 0 ; i < nout; i++)
 		{
-			double si = ((double) buf[i].i);
-			double sq = ((double) buf[i].q);
+			ComplexSample* cSample = &lut[buf[i]];
 
-			SampleFilter_put(&filteri, si);
-			SampleFilter_put(&filterq, sq);
+			SampleFilter_put(&filteri, cSample->i);
+			SampleFilter_put(&filterq, cSample->q);
 
-			si = SampleFilter_get(&filteri);
-			sq = SampleFilter_get(&filterq);
+			double si = SampleFilter_get(&filteri);
+			double sq = SampleFilter_get(&filterq);
 
 			double mag = si*si + sq*sq;
 			avg += mag;
@@ -253,3 +351,4 @@ int main(int argc, char **argv)
 
 	return 0;
 }
+
