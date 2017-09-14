e05404008f0f70ea6f038c83ca15554319254279
Tom Wimmenhove <tom.wimmenhove@gmail.com>
2017-09-14 18:21:15 +0000

    Filter didn't do anything at all. Fixed

--- filter.h
+++ filter.h
@@ -1,26 +1,30 @@
-// Designed using http://t-filter.engineerjs.com/
 #ifndef SAMPLEFILTER_H_
 #define SAMPLEFILTER_H_
 
 /*
 
-   FIR filter designed with
-http://t-filter.appspot.com
+FIR filter designed with
+ http://t-filter.appspot.com
 
 sampling frequency: 1000000 Hz
 
- * 0 Hz - 50000 Hz
- gain = 1
- desired ripple = 5 dB
- actual ripple = 0 dB
+* 0 Hz - 50000 Hz
+  gain = 1
+  desired ripple = 5 dB
+  actual ripple = 4.137265934511618 dB
 
- */
+* 100000 Hz - 500000 Hz
+  gain = 0
+  desired attenuation = -50 dB
+  actual attenuation = -50.07784704888356 dB
 
-#define SAMPLEFILTER_TAP_NUM 5
+*/
+
+#define SAMPLEFILTER_TAP_NUM 31
 
 typedef struct {
-	double history[SAMPLEFILTER_TAP_NUM];
-	unsigned int last_index;
+  double history[SAMPLEFILTER_TAP_NUM];
+  unsigned int last_index;
 } SampleFilter;
 
 void SampleFilter_init(SampleFilter* f);
