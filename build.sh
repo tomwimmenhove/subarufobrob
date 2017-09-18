616b810f27c97df20c7a7c639f60400a291fb372
Tom Wimmenhove <tom.wimmenhove@gmail.com>
2017-09-18 02:04:33 +0000

    Updated build script

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ gcc -O2 -c hex.c
 gcc -O2 -c manchester.c
 gcc -O2 -c protocol.c
 gcc -O2 -c runningavg.c
+gcc -O2 -o rpitxify rpitxify.c hex.o manchester.o
 gcc -O2 -o demod demod.c runningavg.o hex.o protocol.o manchester.o -lpthread -lrtlsdr
 gcc -O2 -o rollthecode rollthecode.c hex.o protocol.o manchester.o
 
