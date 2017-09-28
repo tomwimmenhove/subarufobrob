16fe70439b1675d174b3e920890744cd789d8dc4
Tom Wimmenhove <t.wimmenhove@bond3d.com>
2017-09-28 17:33:33 -0400

    Added GNU licence

--- hex.c
+++ hex.c
@@ -1,3 +1,22 @@
+/*
+ *  Copyright 2017 Tom Wimmenhove<tom.wimmenhove@gmail.com>
+ *
+ *  This file is part of Subarufobrob
+ *
+ *  Subarufobrob is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Subarufobrob is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Subarufobrob.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdint.h>
 #include <stdio.h>
 #include <ctype.h>
